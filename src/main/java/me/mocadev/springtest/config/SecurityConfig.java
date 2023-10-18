329b2eb4f10184582f698c14462f90332064488d
tony <chcjswo@gmail.com>
2023-10-19 01:14:58 +0900

    JwtAuthenticationFilter 등록

--- src/main/java/me/mocadev/springtest/config/SecurityConfig.java
+++ src/main/java/me/mocadev/springtest/config/SecurityConfig.java
@@ -5,10 +5,12 @@ import java.util.List;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.extern.slf4j.Slf4j;
 import me.mocadev.springtest.dto.ResponseDto;
+import me.mocadev.springtest.jwt.JwtAuthenticationFilter;
 import me.mocadev.springtest.util.CustomResponseUtil;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.MediaType;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer;
@@ -29,6 +31,16 @@ public class SecurityConfig {
 		return new BCryptPasswordEncoder();
 	}
 
+	public class CustomSecurityFilterManager extends AbstractHttpConfigurer<CustomSecurityFilterManager, HttpSecurity> {
+
+		@Override
+		public void configure(HttpSecurity builder) throws Exception {
+			AuthenticationManager authenticationManager = builder.getSharedObject(AuthenticationManager.class);
+			builder.addFilter(new JwtAuthenticationFilter(authenticationManager));
+			super.configure(builder);
+		}
+	}
+
 	@Bean
 	public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
 		http.headers(
@@ -42,6 +54,9 @@ public class SecurityConfig {
 		http.formLogin().disable();
 		http.httpBasic().disable();
 
+		http.apply(new CustomSecurityFilterManager());
+
+		// Exception 처리
 		http.exceptionHandling().authenticationEntryPoint((request, response, authException) -> {
 			CustomResponseUtil.unAuthentication(response, "인증되지 않은 사용자입니다.");
 		});
